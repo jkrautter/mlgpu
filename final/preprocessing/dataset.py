2ec2b531c8e35d802a9af6a7b486582914980e1c
Jonas Krautter <jonas@krautter.info>
2017-01-21 13:49:09 +0100

    preprocessing finished

--- final/preprocessing/dataset.py
+++ final/preprocessing/dataset.py
@@ -46,7 +46,7 @@ class DataSet:
     
     def initializeTables(self):
         c = self.conn.cursor()
-        c.execute("CREATE TABLE IF NOT EXISTS `companies` (`cid` INTEGER NOT NULL PRIMARY KEY, `name` TEXT)")
+        c.execute("CREATE TABLE IF NOT EXISTS `companies` (`cid` INTEGER NOT NULL PRIMARY KEY, `num` INTEGER NOT NULL, `name` TEXT)")
         c.execute("CREATE TABLE IF NOT EXISTS `vocab` (`wid` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `word` TEXT)")
         c.execute("CREATE TABLE IF NOT EXISTS `data` (`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " 
                   + "`basecid` INTEGER NOT NULL, `targetcid` INTEGER NOT NULL, `seqvec` BLOB, `len` INTEGER,"
@@ -71,6 +71,7 @@ class DataSet:
         wnum = 2
         cnum = 0
         snum = 0
+        lnum = 0
         wdict = dict()
         cset = set()
         stop = nltk.corpus.stopwords.words("german")
@@ -84,51 +85,70 @@ class DataSet:
             print("Processing file " + f + "...\n")
             datafile = open(f)   
             for line in datafile:
+                lnum += 1
+                if lnum % 10000 == 0:
+                    print("Processing line: " + str(lnum) + "\n")
                 if not "http" in line:
                     companies = []
                     nextcomp = self.findNextCompany(line, 0)
                     tempcnum = 0
                     while nextcomp != -1:
                         if nextcomp["id"] not in cset and "\n" not in nextcomp["name"]:
-                            cnum += 1
-                            t = (nextcomp["id"], nextcomp["name"])
+                            t = (nextcomp["id"], cnum, nextcomp["name"])
                             cset.add(nextcomp["id"])
-                            c.execute("INSERT INTO `companies` VALUES (?, ?)", t)
+                            c.execute("INSERT INTO `companies` VALUES (?, ?, ?)", t)
+                            cnum += 1
                             if cnum % 10000 == 0:
                                 print("Found " + str(cnum) + " companies, continuing...\n")
-                                companies.append(nextcomp)
-                                line = line[:nextcomp["start"]] + "[<[" + str(tempcnum) + "]>]" + line[nextcomp["end"]:]
-                                tempcnum += 1
-                                nextcomp = self.findNextCompany(line, nextcomp["start"] + 6 + math.ceil(math.log10(tempcnum)))
-                                if len(companies) <= self.max_companies:
-                                    snum += 1
-                                    if snum % 10000 == 0:
-                                        print("Processing line: " + str(snum) + "\n")
-                                        for i in range(len(companies)):
-                                            baseline = line.replace("[<[" + str(i) + "]>]", " basecompanyplaceholder ")
-                                            for j in range(len(companies)):
-                                                if i != j:
-                                                    targetline = baseline.replace("[<[" + str(j) + "]>]", " targetcompanyplaceholder ")
-                                                    for k in range(len(companies)):
-                                                        if k != j and k != j:
-                                                            targetline = targetline.replace("[<[" + str(k) + "]>]", companies[k]["name"])
-                                                            targetline = "".join([ch for ch in targetline if ch not in string.punctuation and ch not in string.digits])
-                                                            tokens = nltk.word_tokenize(targetline, "german")
-                                                            tokens = [w for w in tokens if w not in stop]
-                                                            for w in tokens:
-                                                                if w not in wdict:
-                                                                    wdict[w] = wnum
-                                                                    t = (wnum, w,) 
-                                                                    wnum += 1
-                                                                    c.execute("INSERT INTO `vocab` (`wid`, `word`) VALUES (?, ?)", t)
-                                                                    if wnum % 10000 == 0:
-                                                                        print("Found " + str(wnum) + " words, continuing...\n")
-                                                                        seqvec = list(map(lambda x: wdict[x], tokens))
-                                                                        t = (companies[i]["id"], companies[j]["id"], pickle.dumps(seqvec), len(seqvec),)
-                                                                        c.execute("INSERT INTO `data` (`basecid`, `targetcid`, `seqvec`, `len`) VALUES (?, ?, ?, ?)", t)
-                                                                        self.conn.commit()
+                        companies.append(nextcomp)
+                        line = line[:nextcomp["start"]] + "[<[" + str(tempcnum) + "]>]" + line[nextcomp["end"]:]
+                        tempcnum += 1
+                        nextcomp = self.findNextCompany(line, nextcomp["start"] + 6 + math.ceil(math.log10(tempcnum)))
+                    if len(companies) <= self.max_companies:
+                        snum += 1
+                        if snum % 10000 == 0:
+                            print("Lines processed for database: " + str(snum) + "\n")
+                            self.conn.commit()
+                        for i in range(len(companies)):
+                            baseline = line.replace("[<[" + str(i) + "]>]", " basecompanyplaceholder ")
+                            for j in range(len(companies)):
+                                if i != j:
+                                    targetline = baseline.replace("[<[" + str(j) + "]>]", " targetcompanyplaceholder ")
+                                    for k in range(len(companies)):
+                                        if k != j and k != i:
+                                            targetline = targetline.replace("[<[" + str(k) + "]>]", companies[k]["name"])
+                                    targetline = "".join([ch for ch in targetline if ch not in string.punctuation and ch not in string.digits])
+                                    targetline = targetline.lower()
+                                    tokens = nltk.word_tokenize(targetline, "german")
+                                    #tokens = [w for w in tokens if w not in stop]
+                                    for w in tokens:
+                                        if w not in wdict:
+                                            wdict[w] = wnum
+                                            t = (wnum, w,) 
+                                            wnum += 1
+                                            c.execute("INSERT INTO `vocab` (`wid`, `word`) VALUES (?, ?)", t)
+                                            if wnum % 10000 == 0:
+                                                print("Found " + str(wnum) + " words, continuing...\n")                
+                                    seqvec = list(map(lambda x: wdict[x], tokens))
+                                    t = (companies[i]["id"], companies[j]["id"], pickle.dumps(seqvec), len(seqvec),)
+                                    c.execute("INSERT INTO `data` (`basecid`, `targetcid`, `seqvec`, `len`) VALUES (?, ?, ?, ?)", t)
+            self.conn.commit()
             datafile.close()
         print("All files processed, " + str(wnum) + " words, " + str(cnum) + " companies and " + str(snum) + " sentences found.")
+        
+    def getStringRepresentationFor(self, did):
+        t = (did,)
+        c = self.conn.cursor()
+        c.execute("SELECT `seqvec` FROM `data` WHERE `id` = ?", t)
+        result = c.fetchone()
+        seqvec = pickle.loads(result[0])
+        words = []
+        for wid in seqvec:
+            t = (wid,)
+            c.execute("SELECT `word` FROM `vocab` WHERE `wid` = ?", t)
+            result = c.fetchone()
+            words.append(result[0])
+        return str(words)
         
     def getDatasetFor(self, basecompany, max_sequence_length):
         t = (basecompany,)
@@ -137,32 +157,27 @@ class DataSet:
         result = c.fetchone()
         name = result[0]
         print("Selected basecompany " + name + ", loading data...\n")
-        c.execute("SELECT `targetcid`, `seqvec` FROM `data` WHERE `basecid`=?", t)
+        c.execute("SELECT c.`num`, d.`seqvec` FROM `data` AS d JOIN `companies` AS c ON d.`targetcid` = c.`cid` WHERE `basecid`=?", t)
         result = c.fetchall()
-        print("Found " + str(len(result)) + " sentences including this basecompany.\nBuilding vocabulary...\n")
-        numwords = 1
-        numtargets = 0
-        worddict = dict()
-        targetdict = dict()
-        for r in result:
-            if r[0] not in targetdict:
-                targetdict[r[0]] = numtargets
-                numtargets += 1
-            for w in r[1]:
-                if w not in worddict:
-                    worddict[w] = numwords
-                    numwords += 1
-        print("Vocabulary size for this basecompany is " + str(numwords) + ".\n")
-        print("Target size for this basecompany is " + str(numtargets) + ".\n")
+        print("Found " + str(len(result)) + " sentences including this basecompany.\n")
+        c.execute("SELECT COUNT(`wid`) FROM `vocab`")
+        tempresult = c.fetchone()
+        numwords = int(tempresult[0])
+        c.execute("SELECT COUNT(`cid`) FROM `companies`")
+        tempresult = c.fetchone()
+        numtargets = int(tempresult[0])
         targets = []
         sequences = []
         for r in result:
             target = numpy.zeros(numtargets, dtype=numpy.float32)
-            target[targetdict[r[0]]] = 1.0
+            target[r[0]] = 1.0
             targets.append(target)
-            wordlist = numpy.zeros(max_sequence_length, dtype=numpy.int32)
-            for i in range(min(max_sequence_length, len(r[1]))):
-                wordlist[i] = worddict[r[1][i]]
+            wordlist = pickle.loads(r[1])
+            if len(wordlist) > max_sequence_length:
+                wordlist = wordlist[0:max_sequence_length]
+            else:
+                for j in range(max_sequence_length - len(wordlist)):
+                    wordlist.append(numwords)
             sequences.append(wordlist)
         return {"targets": targets, "sequences": sequences}
         
