bf7a7e29cdd3ce9e9e9d897b2276c8d40d6177ff
Jonas Krautter <huzzf@gmx.de>
2017-02-05 18:42:43 +0100

    changed representation output

--- final/representation.py
+++ final/representation.py
@@ -43,13 +43,17 @@ def main():
         output_feed = [model.representation]
         outputs = sess.run(output_feed, input_feed)
         count = 0
-        for o in outputs:
+        for o in outputs[0][0]:
             t = (str(dataset["ids"][count]), str(pickle.dumps(o, 0)))
-            if ofile is not None:
-                for o in outputs:
-                    for i in o:
-                        ofile.write(bytes(i))
-                ofile.close()
+	    count += 1
+        if ofile is not None:
+	    count = 0
+            for o in outputs[0][0]:
+		ofile.write(bytes(dataset["ids"][count]))
+                for i in o:
+                    ofile.write(bytes(i))
+		count += 1
+        ofile.close()
         c.execute("INSERT INTO `vectors` VALUES (?, ?)", t)
         conn.commit()
 
